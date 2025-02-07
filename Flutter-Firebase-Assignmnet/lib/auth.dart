b01510f971350a7091fc979f514613b96f0c8e6a
Syed Muhammad Zain Sarfraz <130450295+Zainsar@users.noreply.github.com>
2025-02-07 13:11:46 +0500

    Updated

---
+++ Flutter-Firebase-Assignmnet/lib/auth.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+import 'login_screen.dart';  // Import Login Screen
+
+class AuthScreen extends StatefulWidget {
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _name = '';
+  String _email = '';
+  String _password = '';
+  String _phoneNo = '';
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  final nameValidator = RequiredValidator(errorText: 'Name is required');
+  final emailValidator = MultiValidator([RequiredValidator(errorText: 'Email is required'), EmailValidator(errorText: 'Enter a valid email address')]);
+  final passwordValidator = RequiredValidator(errorText: 'Password is required');
+  final phoneValidator = MultiValidator([RequiredValidator(errorText: 'Phone number is required'), MinLengthValidator(10, errorText: 'Enter a valid phone number')]);
+
+  bool _isPasswordVisible = false; // Variable to control password visibility
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign Up'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Name Input
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Name'),
+                onChanged: (value) => _name = value,
+                validator: nameValidator,
+              ),
+              SizedBox(height: 16),
+
+              // Email Input
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Email'),
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) => _email = value,
+                validator: emailValidator,
+              ),
+              SizedBox(height: 16),
+
+              // Password Input with Eye Icon
+              TextFormField(
+                obscureText: !_isPasswordVisible, // Toggle the visibility of the password
+                onChanged: (value) => _password = value,
+                validator: passwordValidator,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                      color: _isPasswordVisible ? Colors.blue : Colors.grey,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible; // Toggle the password visibility
+                      });
+                    },
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+
+              // Phone Number Input
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Phone Number'),
+                keyboardType: TextInputType.phone,
+                onChanged: (value) => _phoneNo = value,
+                validator: phoneValidator,
+              ),
+              SizedBox(height: 16),
+
+              // Sign Up Button
+              ElevatedButton(
+                onPressed: _handleSignUp,
+                child: Text('Sign Up'),
+              ),
+              SizedBox(height: 16),
+
+              // Go to Login Page
+              TextButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()), // Navigate to Login
+                  );
+                },
+                child: Text("Already have an account? Log in"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _handleSignUp() async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        // Create user using email and password
+        UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+          email: _email,
+          password: _password,
+        );
+
+        // Store additional information (Name, Phone Number) in Firebase Firestore
+        await FirebaseFirestore.instance.collection('users').doc(userCredential.user?.uid).set({
+          'name': _name,
+          'phone': _phoneNo,
+          'email': _email,
+        });
+
+        // Navigate to Login Page after SignUp
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => LoginScreen()),  // Go to Login
+        );
+
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Successfully signed up!')));
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Error: ${e.toString()}')));
+      }
+    }
+  }
+}
---
+++ Flutter-Firebase-Assignmnet/lib/login_screen.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_assignment/main.dart';
+import 'package:form_field_validator/form_field_validator.dart'; // Import main screen
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _email = '';
+  String _password = '';
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  final emailValidator = MultiValidator([
+    RequiredValidator(errorText: 'Email is required'),
+    EmailValidator(errorText: 'Enter a valid email address')
+  ]);
+  final passwordValidator = RequiredValidator(errorText: 'Password is required');
+
+  bool _isPasswordVisible = false; // Variable to control password visibility
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Email Input
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Email'),
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) => _email = value,
+                validator: emailValidator,
+              ),
+              SizedBox(height: 16),
+
+              // Password Input with Eye Icon
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                      color: _isPasswordVisible ? Colors.blue : Colors.grey,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible; // Toggle the password visibility
+                      });
+                    },
+                  ),
+                ),
+                obscureText: !_isPasswordVisible, // Toggle visibility of the password
+                onChanged: (value) => _password = value,
+                validator: passwordValidator,
+              ),
+              SizedBox(height: 16),
+
+              // Login Button
+              ElevatedButton(
+                onPressed: _handleLogin,
+                child: Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _handleLogin() async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        // Login user using email and password
+        await _auth.signInWithEmailAndPassword(email: _email, password: _password);
+
+        // If login is successful, navigate to the main screen
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => MyApp()),  // Go to Main Screen
+        );
+
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Successfully logged in!')));
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Error: ${e.toString()}')));
+      }
+    }
+  }
+}
